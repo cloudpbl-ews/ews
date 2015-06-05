c3011882bb608a1533c6b88532fe39ed2e6ab390
Tomoya Chiba <tomo.asleep@gmail.com>
2015-06-05 19:12:49 +0900

    Add login and logout pages

--- ews/settings/base.py
+++ ews/settings/base.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'ews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -91,3 +91,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/profile'
+LOGIN_URL = '/'
