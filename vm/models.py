689b93f807c183396e28521a6d25d4c4d4da6c2f
Tomoya Chiba <tomo.asleep@gmail.com>
2015-06-05 19:50:45 +0900

    Add `name` to VirtualMachine

--- vm/models.py
+++ vm/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class VirtualMachine(models.Model):
+    name = models.CharField(max_length=100, default='your virtual machine')
     uuid = models.CharField(max_length=100)
     user = models.ForeignKey(User)
 
+    def __unicode__(self):
+        return self.name
