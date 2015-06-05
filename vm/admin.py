ef9c5fb75d698b6672c6884ca6a2e296db7b3128
Hiromasa Ihara <iharahiromasa@gmail.com>
2015-06-05 20:12:25 +0900

    VMのCURD adminページを追加

--- vm/admin.py
+++ vm/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import VirtualMachine
 
-admin.site.register(VirtualMachine)
+class VMAdmin(admin.ModelAdmin):
+  # 表示レイアウト
+  fieldsets = [(None, { 'fields': ['name', 'uuid', 'user'] })]
+  # 表示項目
+  list_display = ('uuid', 'user')
+  # フィルタ項目
+  list_filter = ['user']
+
+admin.site.register(VirtualMachine, VMAdmin)
