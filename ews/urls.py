5b37183f7ae13d74cb85f3d31386365732c22407
Hiromasa Ihara <iharahiromasa@gmail.com>
2015-06-13 17:16:05 +0900

    rootのindex pageを追加

--- ews/urls.py
+++ ews/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'django.contrib.auth.views.login',
         {'template_name': 'accounts/login.html'}),
