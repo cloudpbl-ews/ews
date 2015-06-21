1e74cc0f9404d058d1727295d3d4eaf0817de06e
ramesses2nd <small.whale.226@gmail.com>
2015-06-21 15:27:00 +0900

    名前の取得、状態を取得する関数を追加 また一部の変数、クラス名を変更

--- vm/vmoperation/vm_network.py
+++ vm/vmoperation/vm_network.py
@@ -4,51 +4,77 @@ from uuid import UUID
 from xml.dom import minidom # xml parcer module
 
 
+# CONSTANTS #
+NETWORK_AUTOSTART = 1
+NETWORK_NO_AUTOSTART = 0
+
+
 # networks on the host
-class HostNetwork():
+class HostVirtualNetworks():
 
-	def __init__(self, uri):
-		self.conn = libvirt.open(uri)
+    def __init__(self, uri):
+        self.conn = libvirt.open(uri)
 
-	# Return a list of VirtualNetwork objects
-	def get_list_networks(self):
-		vnw_list = []
-		for vnw in self.conn.listAllNetworks():
-			vnw_list.append( VirtualNetwork(vnw) )
-		return vnw_list
+    # return a list of VirtualNetwork objects
+    def get_list_networks(self):
+        virt_nw_list = []
+        for virt_nw in self.conn.listAllNetworks():
+            virt_nw_list.append( VirtualNetwork(virt_nw) )
+        return virt_nw_list
 
 class VirtualNetwork():
 
-	def __init__(self, vnw):
-		self.vnw = vnw
+    def __init__(self, virt_nw):
+        self.virt_nw = virt_nw
 
-	def start(self):
-		if not self.vnw.isActive():
-			self.vnw.create()
+    def start(self):
+        if not self.virt_nw.isActive():
+            self.virt_nw.create()
+        else:
+            raise Exception("Already Running")
 
-	# this function needs privileged access
-	def stop(self):
-		if self.vnw.isActive():
-			self.vnw.destroy()
+    # this function needs privileged access
+    def stop(self):
+        if self.virt_nw.isActive():
+            self.virt_nw.destroy()
+        else:
+            raise Exception("Already Stopping")
 
-	def delete(self):
-		if not self.vnw.isActive():
-			self.vnw.undefine()
+    def delete(self):
+        if not self.virt_nw.isActive():
+            self.virt_nw.undefine()
+            self.virt_nw = None
+        else:
+            raise Exception("Not Stopping")
 
-	def is_auto(self):
-		if self.vnw.autostart() == libvirt.VIR_CONNECT_LIST_NETWORKS_AUTOSTART:
-			return True
-		elif self.vnw.autostart() == libvirt.VIR_CONNECT_LIST_NETWORKS_NO_AUTOSTART:
-			return False
-		else:
-			raise
+    def is_auto(self):
+        if self.virt_nw.autostart() == 1: # 
+            return True
+        else:
+            return False
 
-	def name(self):
-		return self.vnw.name()
+    # auto_val is NETWORK_AUTOSTART or NETWORK_NO_AUTOSTART
+    def set_auto(self, auto_val):
+        self.virt_nw.setAutostart(auto_val)
+
+
+    def is_running(self):
+        return self.virt_nw.isActive()
+
+    # return the public name for that network. Example: default
+    def name(self):
+        return self.virt_nw.name()
+
+    # return a bridge interface name to which a domain may connect a network interface in order to join the network. Example: br0
+    def br_name(self):
+        return self.virt_nw.bridgeName()
 
 
 # main() is for debug
 if __name__ == '__main__':
-	host_nw = HostNetwork("lxc:///")
-	for vnw in host_nw.get_list_networks():
-		print vnw.name()
+    host_nw = HostVirtualNetworks("lxc:///")
+    for nw in host_nw.get_list_networks():
+        print nw.br_name(), nw.name()
+        nw.set_auto(1)
+
+
