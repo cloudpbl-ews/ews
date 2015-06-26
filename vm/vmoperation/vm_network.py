eb77e5f9efb1990b24b899cce73b43d949b53ab0
ramesses2nd <small.whale.226@gmail.com>
2015-06-26 18:46:05 +0900

    仮想ネットワークの設定、define, createの実装

--- vm/vmoperation/vm_network.py
+++ vm/vmoperation/vm_network.py
@@ -2,6 +2,7 @@ import libvirt
 from uuid import UUID
 
 from xml.dom import minidom # xml parcer module
+from xml.etree import ElementTree as ET
 
 
 # CONSTANTS #
@@ -21,6 +22,12 @@ class HostVirtualNetworks():
         for virt_nw in self.conn.listAllNetworks():
             virt_nw_list.append( VirtualNetwork(virt_nw) )
         return virt_nw_list
+
+    def define_network_xml(self, net_xml):
+        self.conn.networkCreateXML(net_xml)
+
+    def create_network_xml(self, net_xml):
+        self.conn.networkCreateXML(net_xml)
 
 class VirtualNetwork():
 
@@ -69,6 +76,30 @@ class VirtualNetwork():
     def br_name(self):
         return self.virt_nw.bridgeName()
 
+# net_name: the name of a virtual network, 
+def gen_network_xml(net_name, br_name, ip_addr, net_mask, start_range, end_range):
+    root = ET.Element('network')
+    # set network's name
+    name = ET.SubElement(root, 'name')
+    name.text = net_name
+    # set forward. Now fix nat
+    forward = ET.SubElement(root, 'forward')
+    forward.set('mode', 'nat')
+    # set bridge name
+    bridge = ET.SubElement(root, 'bridge')
+    bridge.set('name', br_name)
+    # set ip address and netmask
+    ip = ET.SubElement(root, 'ip')
+    ip.set('address', ip_addr)
+    ip.set('netmask', net_mask)
+    # set dhcp range
+    dhcp = ET.SubElement(ip, 'dhcp')
+    dhcp_range = ET.SubElement(dhcp, 'range')
+    dhcp_range.set('start', start_range)
+    dhcp_range.set('end', end_range)
+    return ET.tostring(root)
+
+        
 
 # main() is for debug
 if __name__ == '__main__':
