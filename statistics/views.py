bb3faed167456b2feab619c36483ef87ac212c93
motomuman <motomu@hongo.wide.ad.jp>
2015-06-28 17:39:50 +0900

    delete unneccessary change

--- statistics/views.py
+++ statistics/views.py
@@ -1,7 +1,3 @@
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 # Create your views here.
-@login_required
-def index(request):
-    return HttpResponse('success')
