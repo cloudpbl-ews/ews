504da1d3e8b049bce627e1e464a17c860773bcaf
Tomoya Chiba <tomo.asleep@gmail.com>
2015-07-03 17:10:12 +0900

    Add switch routine of info and edit for editable cells

--- static/js/application.js
+++ static/js/application.js
@@ -1,3 +1,24 @@
+var EditableCell = (function (){
+    var klass = function EditableCell($el) {
+        this.$el = $el;
+        this.listenClick();
+    }
+
+    klass.prototype.listenClick = function () {
+        var self = this
+        this.$el.find('.switch-edit').on('click', function (ev) {
+            self.toggle();
+            return false;
+        });
+    }
+
+    klass.prototype.toggle = function () {
+        this.$el.find('.editable-cell-info, .editable-cell-edit').toggle();
+    }
+
+    return klass;
+}());
+
 $(function () {
     $('form.form-danger').submit(function () {
         return confirm($(this).data('confirm'));
@@ -5,5 +26,9 @@ $(function () {
 
     $('.selectpicker').selectpicker({
         'selectedText': 'cat',
+    });
+
+    $('.editable-cell').each(function () {
+        window.lastcell = new EditableCell($(this));
     });
 });
